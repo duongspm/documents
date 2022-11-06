8f6f22fe4a5b8bdd9ab1267493e0976b7dd0bd2b
Trần Văn Dương <56970251+duongspm@users.noreply.github.com>
2022-11-06 23:21:29 +0700

    Add files via upload

---
+++ frontend/css/filter/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="gallery">
+        <div class="container">
+            
+
+            <div class="row row2">
+                <div class="gallery-item cafe">
+                    <div class="gallery-item-inner">
+                        <img src="https://images.unsplash.com/photo-1447933601403-0c6688de566e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y29mZmVlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="cafe">
+                    </div>
+                </div>
+
+                <div class="gallery-item shoe">
+                    <div class="gallery-item-inner">
+                        <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="sheo">
+                    </div>
+                </div>
+                <div class="gallery-item shoe">
+                    <div class="gallery-item-inner">
+                        <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="sheo">
+                    </div>
+                </div>
+                <div class="gallery-item headphone">
+                    <div class="gallery-item-inner">
+                        <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVhZHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="headphone">
+                    </div>
+                </div>
+                <div class="gallery-item cam">
+                    <div class="gallery-item-inner">
+                        <img src="https://images.unsplash.com/photo-1516035069371-29a1b244cc32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FtZXJhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="cam">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row row1">
+                <div class="gallery-filter">
+                    <span class="filter-item active" data-filter="all">All</span> 
+
+                    <span class="filter-item" data-filter="cafe">Cafe</span>
+                    <span class="filter-item" data-filter="shoe">shoe</span>
+                    <span class="filter-item" data-filter="headphone">headphone</span>
+                    <span class="filter-item" data-filter="cam">Cam</span>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
